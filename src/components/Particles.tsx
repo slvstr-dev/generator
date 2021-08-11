09910528ace72f04c617c5e5586ecd2da3a37346
slvstr.dev <hello@slvstr.dev>
2021-08-11 10:35:47 +0200

    Refactored Particles component + improved Typescript usage.

--- src/components/Particles.tsx
+++ src/components/Particles.tsx
@@ -1,40 +1,45 @@
+// Adjusted/rewritten component based on: https://codepen.io/FlorinPop17/full/wvvrbmY
 import Sketch from "react-p5";
 import p5Types from "p5";
 
+type Particle = {
+	position: p5Types.Vector;
+	velocity: p5Types.Vector;
+	draw: () => void;
+	update: () => void;
+	connectParticles: (particles: Particle[]) => void;
+};
+
 const Particles: React.FC = () => {
-	let particles: any = [];
+	let particles: Particle[] = [];
 
 	const setup = (p5: p5Types, canvasParentRef: Element) => {
 		p5.createCanvas(window.innerWidth, window.innerHeight).parent(
 			canvasParentRef
 		);
 
-		const particlesLength = Math.min(
+		const particlesAmount = Math.min(
 			Math.floor(window.innerWidth / 10),
 			100
 		);
 
-		for (let i = 0; i < particlesLength; i++) {
+		for (let i = 0; i < particlesAmount; i++) {
 			particles.push({
 				position: p5.createVector(
 					p5.random(p5.width),
 					p5.random(p5.height)
 				),
 				velocity: p5.createVector(
-					p5.random(-0.5, 0.5),
-					p5.random(-0.5, 0.5)
+					p5.random(-0.2, 0.2),
+					p5.random(-0.2, 0.2)
 				),
-				size: 5,
-				update() {
-					this.position.add(this.velocity);
-					this.edges();
-				},
 				draw() {
 					p5.noStroke();
-					p5.fill("rgba(253, 216, 112, .5)");
-					p5.circle(this.position.x, this.position.y, this.size * 2);
+					p5.fill("rgb(208, 144, 47)");
+					p5.circle(this.position.x, this.position.y, 5);
 				},
-				edges() {
+				update() {
+					this.position.add(this.velocity);
 					if (this.position.x < 0 || this.position.x > p5.width) {
 						this.velocity.x *= -1;
 					}
@@ -43,19 +48,17 @@ const Particles: React.FC = () => {
 						this.velocity.y *= -1;
 					}
 				},
-				checkParticles(particles: any) {
-					particles.forEach((particle: any) => {
-						const d = p5.dist(
+				connectParticles(particles) {
+					particles.forEach((particle) => {
+						const distance = p5.dist(
 							this.position.x,
 							this.position.y,
 							particle.position.x,
 							particle.position.y
 						);
 
-						if (d < 120) {
-							const alpha = p5.map(d, 0, 120, 0, 0.25);
-
-							p5.stroke(`rgba(255, 246, 159, ${alpha})`);
+						if (distance < 100) {
+							p5.stroke("rgba(255, 246, 159, .2)");
 							p5.line(
 								this.position.x,
 								this.position.y,
@@ -72,20 +75,14 @@ const Particles: React.FC = () => {
 	const draw = (p5: p5Types) => {
 		p5.background("rgb(53, 20, 9)");
 
-		particles.forEach((particle: any, idx: any) => {
+		particles.forEach((particle, index) => {
 			particle.update();
 			particle.draw();
-			particle.checkParticles(particles.slice(idx));
+			particle.connectParticles(particles.slice(index));
 		});
 	};
 
-	return (
-		<Sketch
-			className="fixed top-0 left-0 right-0 bottom-0 z-0"
-			setup={setup}
-			draw={draw}
-		/>
-	);
+	return <Sketch className="fixed inset-0" setup={setup} draw={draw} />;
 };
 
 export default Particles;
