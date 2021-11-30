757558ecfcd06cdb0875660aeaed0533f6067783
slvstr.dev <hello@slvstr.dev>
2021-11-30 14:39:58 +0100

    Updates

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header: React.FC = () => (
-	<header>
-		<h1 className="text-center text-deus-lightest text-3xl uppercase">
-			Password generator
-		</h1>
-	</header>
-);
-
-export default Header;
